69449007bb5bac4831ebe9fd24277df13fd457f2
M. Humayon Kobir <azkaban@Ms-MacBook-Air.local>
2024-12-18 00:09:45 +0600

    api url used for all the thunk function

--- src/redux/todos/thunk/deleteTodo.js
+++ src/redux/todos/thunk/deleteTodo.js
@@ -1,8 +1,10 @@
 import { deleted } from "../actions";
 
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:9000";
+
 const deleteTodo = (todoId) => {
     return async (dispatch) => {
-        await fetch(`http://localhost:9000/todos/${todoId}`, {
+        await fetch(`${API_URL}/todos/${todoId}`, {
             method: "DELETE",
         });
 
--- src/redux/todos/thunk/updateColor.js
+++ src/redux/todos/thunk/updateColor.js
@@ -1,8 +1,10 @@
 import { colorSelected } from "../actions";
 
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:9000";
+
 const updateColor = (todoId, color) => {
     return async(dispatch) => {
-        const response = await fetch (`http://localhost:9000/todos/${todoId}`, {
+        const response = await fetch (`${API_URL}/todos/${todoId}`, {
             method: "PATCH",
             body: JSON.stringify({
                 color: color,
